f5deb165d9b785d6efbf3f107803ce61282ab7e6
Morphen <daemonsea@gmail.com>
2021-09-30 23:06:27 -0500

    arreglo de errores

--- src/screens/tasks/Tasks.js
+++ src/screens/tasks/Tasks.js
@@ -1,20 +1,20 @@
 import { AuthContext } from "../../provider/AuthProvider";
-import { StatusBar } from 'expo-status-bar';
-import React, {useState, useContext} from 'react';
-import { Keyboard, 
-         KeyboardAvoidingView, 
-         Platform, StyleSheet, 
-         Text, 
-         TextInput, 
-         TouchableOpacity, 
-         View
-} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useContext } from "react";
 import {
-    Button
-} from "react-native-rapi-ui";
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View
+} from "react-native";
+import { Button } from "react-native-rapi-ui";
 import useFetch from "../../hooks/useFetch";
 const API = "https://habitapp-backend.herokuapp.com/users/";
-import Card_task from './card_task/Card_task';
+import Card_task from "./card_task/Card_task";
 import * as firebase from "firebase";
 
 export default function Task() {
@@ -22,59 +22,63 @@ export default function Task() {
   const [info, loading] = useFetch(API + data.id, "", "GET");
   const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
-  
+
   const handelAddTask = () => {
     Keyboard.dismiss();
-    setTaskItems([...taskItems, task])
+    setTaskItems([...taskItems, task]);
     setTask(null);
-  }
+  };
 
   const completeTask = (index) => {
     let itemsCopy = [...taskItems];
     itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy)
-  }
+    setTaskItems(itemsCopy);
+  };
 
   return (
     <View style={styles.container}>
-        <Button
-              status="danger"
-              text="Logout"
-              onPress={() => {
-                firebase.auth().signOut();
-              }}
-              style={{
-                marginTop: 10,
-              }}
-            />
+      <Button
+        status="danger"
+        text="Logout"
+        onPress={() => {
+          firebase.auth().signOut();
+        }}
+        style={{
+          marginTop: 10
+        }}
+      />
       <View style={styles.tasksWrapper}>
-        <Text style={styles.sectionTitle}>Today's tasks {info.username} </Text>
+        {!loading && <Text> {info.username} </Text>}
+        <Text style={styles.sectionTitle}>Today's tasks </Text>
 
         <View style={styles.items}>
-          {
-            taskItems.map((item, index) => {
-              return (
-                <TouchableOpacity key={index}  onPress={() => completeTask(index)}>
-                  <Card_task text={item} /> 
-                </TouchableOpacity>
-              )
-            })
-          }
+          {taskItems.map((item, index) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Card_task text={item} />
+              </TouchableOpacity>
+            );
+          })}
         </View>
       </View>
 
       <KeyboardAvoidingView
-        behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
-        style={styles.writeTaskWrapper}>
-        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}></TextInput>
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Write a task"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        ></TextInput>
 
-        <TouchableOpacity onPress = { () => handelAddTask()}>
+        <TouchableOpacity onPress={() => handelAddTask()}>
           <View style={styles.addWrapper}>
             <Text style={styles.addText}>+</Text>
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
-      
     </View>
   );
 }
@@ -82,45 +86,45 @@ export default function Task() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E8EAED'
+    backgroundColor: "#E8EAED"
   },
   tasksWrapper: {
     paddingTop: 80,
-    paddingHorizontal: 20,
+    paddingHorizontal: 20
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
   items: {
-    marginTop: 30,
+    marginTop: 30
   },
   writeTaskWrapper: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 20,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center'
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
   },
   input: {
     paddingVertical: 15,
     paddingHorizontal: 15,
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 60,
-    borderColor: '#C0C0C0',
+    borderColor: "#C0C0C0",
     borderWidth: 1,
     width: 280
   },
   addWrapper: {
     width: 60,
     height: 60,
-    backgroundColor: '#FFF',
+    backgroundColor: "#FFF",
     borderRadius: 60,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderColor: '#C0C0C0',
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
     borderWidth: 1
   },
-  addText: {},
+  addText: {}
 });
