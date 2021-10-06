8f63231268febe05662b6f2224c80d695c2d572a
Jenny980 <jennytangarife98@gmail.com>
2021-10-06 17:41:17 -0500

    Revert "estadisticas"
    This reverts commit a43724c56949dda87c1bfb0c44738c1379834f8f.

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AuthContext } from "../provider/AuthProvider";
-import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 // Main
 import Home from "../screens/tasks/Home";
@@ -12,16 +12,15 @@ import Task from "../screens/tasks/Tasks";
 import SecondScreen from "../screens/tasks/SecondScreen";
 import Statistics from "../screens/statistics/Statistics";
 
+
 // Auth screens
 import Login from "../screens/auth/Login";
 import Register from "../screens/auth/Register";
 import ForgetPassword from "../screens/auth/ForgetPassword";
 
 import Loading from "../screens/utils/Loading";
-import useFetch from "../hooks/useFetch";
-// Better put your these secret keys in .env file
-const API = "https://habitapp-backend.herokuapp.com/users/";
 
+// Better put your these secret keys in .env file
 const firebaseConfig = {
   apiKey: "AIzaSyByZiObxT3jhwOpJVpyJr-PX453jq-Nbbg",
   authDomain: "habitapp-4056f.firebaseapp.com",
@@ -29,7 +28,7 @@ const firebaseConfig = {
   projectId: "habitapp-4056f",
   storageBucket: "habitapp-4056f.appspot.com",
   messagingSenderId: "554460473425",
-  appId: "1:554460473425:web:6e5484186bed8a5d203070",
+  appId: "1:554460473425:web:6e5484186bed8a5d203070"
 };
 if (firebase.apps.length === 0) {
   firebase.initializeApp(firebaseConfig);
@@ -41,7 +40,7 @@ const Auth = () => {
   return (
     <AuthStack.Navigator
       screenOptions={{
-        headerShown: false,
+        headerShown: false
       }}
     >
       <AuthStack.Screen name="Login" component={Login} />
@@ -54,28 +53,15 @@ const Auth = () => {
 const MainStack = createStackNavigator();
 
 const Main = () => {
-  const data = useContext(AuthContext);
-  const [info, loading] = useFetch(API + data.id + "/tasks/", "", "GET");
-
   return (
     <Tab.Navigator>
-      <Tab.Screen
-        options={{
-          tabBarLabel: "Task",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-        name="Task"
-        component={Task}
-      />
+      <Tab.Screen options={{
+        tabBarLabel: "Task",
+        tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />) }} name="Task" component={Task} />
       <Tab.Screen name="SecondScreen" component={SecondScreen} />
-      {!loading && (
-        <Tab.Screen name="Statistics">
-          {() => <Statistics tasks={info} />}
-        </Tab.Screen>
-      )}
+      <Tab.Screen name="Statistics" component={Statistics} />
     </Tab.Navigator>
+
   );
 };
 
